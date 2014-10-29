38ddfc87f4f17225275b36542338327f557ab379
Ethan Thomas <ethanthomas33@gmail.com>
2014-10-29 13:45:35 -0400

    Initial Commit

---
+++ New HF Navigator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  New HF Navigator
+//
+//  Created by Ethan on 10/29/14.
+//  Copyright (c) 2014 Obsidian Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ New HF NavigatorTests/New_HF_NavigatorTests.m
@@ -0,0 +1,40 @@
+//
+//  New_HF_NavigatorTests.m
+//  New HF NavigatorTests
+//
+//  Created by Ethan on 10/29/14.
+//  Copyright (c) 2014 Obsidian Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface New_HF_NavigatorTests : XCTestCase
+
+@end
+
+@implementation New_HF_NavigatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
